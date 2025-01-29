072e3565d033dc2b8ebdcc528751cb71fcb56080
LisethMecatl <mecatl.toxcoyoa.liseth.1a@gmail.com>
2025-01-29 17:17:05 -0600

    Se creo el archivo codephp.php en la carpeta p03

---
+++ practicas/p03/codephp.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD// XHTML 1.1//EN"
+    <html xmlns="http://www.w3.org/1999/xhtml" xml_lang="es">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UFT-8" />
+    <title>Una pagina PHP</title>
+</head>
+
+<body>
+    <?php
+    echo "<h3> El dia de hoy es el " . date('d/M/Y H:i:s') . "</h3> <hr/>";
+    echo "<H2> Bbienvenido a mi sitio PHP 5 </h2>"
+    ?>
+</body>
+
+</html>
