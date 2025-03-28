edcfe9741d3927acd8ed60dc5ba7a9040fe6e770
LisethMecatl <mecatl.toxcoyoa.liseth.1a@gmail.com>
2025-03-28 15:42:45 -0600

    Validacion de datos en archivo set_producto_v2.php

--- practicas/p08/set_producto_v2.php
+++ practicas/p08/set_producto_v2.php
@@ -1,6 +1,6 @@
 <?php
 // SE CREA EL OBJETO DE CONEXION 
-@$link = new mysqli('localhost', 'root', '12345678910', 'marketzone');
+@$link = new mysqli('localhost', 'root', '', 'marketzone');
 
 //comprobacion de conexión
 if ($link->connect_errno) {
@@ -8,26 +8,45 @@ if ($link->connect_errno) {
     //IMPORTANTE: con @ se suprime el Warning para gestionar el error por medio de código 
 }
 
+//Verificacion de que llegan datos
+/*echo "<pre>";
+print_r($_POST);
+echo "</pre>";*/
+
 
 //Obtener datos del form
-$nombre = $_POST['nombre'] ?? '';
-$marca = $_POST['marca'] ?? '';
-$modelo = $_POST['modelo'] ?? '';
+$nombre = isset($_POST['nombre']) ? trim($_POST['nombre']) : '';
+$marca = isset($_POST['marca']) ? trim($_POST['marca']) : '';
+$modelo = isset($_POST['modelo']) ? trim($_POST['modelo']) : '';
+
 $precio = $_POST['precio'] ?? '';
 $detalles = $_POST['detalles'] ?? '';
-$unidades = $_POST['unidades'] ?? '';
+$unidades = $_POST['unidades'] ?? 0;
 $imagen = 'p08/img/imagen.png';
 
 
 //validacion de campos no vacios
-if (empty($nombre) || empty($modelo)) {
-    die('<h3>Error: Nombre, Marca y Modelo obligatorios.</h3>');
+if (empty($nombre) || empty($marca) || empty($modelo)) {
+    exit("<h3 style='color: red;'>Error: Nombre, Marca y Modelo son obligatorios.</h3>");
 }
 
 
 
-$insertar_producto = "INSERT INTO productos (nombre, marca, modelo, precio, detalles, unidades, imagen) VALUES (?, ?, ?, ?, ?, ?, ?)";
-$stmt = $link->prepare($insertar_producto);
+// Verificar si el producto ya existe
+$sql_check = "SELECT id FROM productos WHERE nombre = ? AND marca = ? AND modelo = ?";
+$stmt = $link->prepare($sql_check);
+$stmt->bind_param("sss", $nombre, $marca, $modelo);
+$stmt->execute();
+$stmt->store_result();
+
+if ($stmt->num_rows > 0) {
+    die('<h3>Error: El producto ya está registrado.</h3>');
+}
+$stmt->close();
+
+// Insertar el producto si no existe
+$sql_insert = "INSERT INTO productos (nombre, marca, modelo, precio, detalles, unidades, imagen) VALUES (?, ?, ?, ?, ?, ?, ?)";
+$stmt = $link->prepare($sql_insert);
 $stmt->bind_param("sssdsis", $nombre, $marca, $modelo, $precio, $detalles, $unidades, $imagen);
 
 if ($stmt->execute()) {
