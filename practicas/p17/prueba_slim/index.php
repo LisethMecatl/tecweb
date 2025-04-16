e2f48e5286341ea5eadc3adb9515eb03582a6184
LisethMecatl <mecatl.toxcoyoa.liseth.1a@gmail.com>
2025-04-15 22:42:37 -0600

    Servicio de PHP con Slim (paso de parametros), segundo video

--- practicas/p17/prueba_slim/index.php
+++ practicas/p17/prueba_slim/index.php
@@ -1,16 +1,25 @@
 <?php
 
-use Psr\Http\Message\ResponseInterface as response;
-use Psr\Http\Message\ServerRequestInterface as request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require 'vendor/autoload.php';
 $app = AppFactory::create();
+
 $app->setBasePath("/tecweb/practicas/p17/prueba_slim");
+
+//activar debug
+//$app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response, $args) {
     $response->getBody()->write("Hola mundo Slim!!");
     return $response;
 });
 
+$app->get("/hola[/{nombre}]", function ($request, $response, $args) {
+    $nombre = $args['nombre'] ?? 'invitado'; // valor por defecto si no se pasa
+    $response->getBody()->write("Hola, " . $args["nombre"]);
+    return $response;
+});
 $app->run();
