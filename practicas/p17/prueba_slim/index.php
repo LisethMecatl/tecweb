6e29a767ee685161a34ddfec57b13cae33e7795f
LisethMecatl <mecatl.toxcoyoa.liseth.1a@gmail.com>
2025-04-17 20:16:58 -0600

    Servicio web php con slim, salida JSON. video 4

--- practicas/p17/prueba_slim/index.php
+++ practicas/p17/prueba_slim/index.php
@@ -17,9 +17,31 @@ $app->get('/', function ($request, $response, $args) {
     return $response;
 });
 
+
+//paso de parametros
 $app->get("/hola[/{nombre}]", function ($request, $response, $args) {
     $nombre = $args['nombre'] ?? 'invitado'; // valor por defecto si no se pasa
     $response->getBody()->write("Hola, " . $args["nombre"]);
+    return $response;
+});
+
+
+//Post
+$app->post("/pruebapost", function ($request, $response, $args) {
+    $reqPost = $request->getParsedBody();
+    $val1 = $reqPost["val1"];
+    $val2 = $reqPost["val2"];
+
+    $response->getBody()->write("Valores:" . $val1 . " " . $val2);
+    return $response;
+});
+
+$app->get("/testjson", function ($request, $response, $args) {
+    $data[0]["nombre"] = "Sergio";
+    $data[0]["apellido"] = "Juarez Perez";
+    $data[1]["nombre"] = "Liseth";
+    $data[1]["apellido"] = "Mecatl Toxcoyoa";
+    $response->getBody()->write(json_encode($data, JSON_PRETTY_PRINT));
     return $response;
 });
 $app->run();
